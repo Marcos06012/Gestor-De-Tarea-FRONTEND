0da51d96580861235a12707401178f97e4cb9c12
MARCOS JARED ALAS MORALES <marcos.alas24@itca.edu.sv>
2026-05-28 16:06:07 -0600

    Fix: Corrigiendo URL de Render real sin guiones

--- src/pages/api/AxiosConfig.js
+++ src/pages/api/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://gestor-tareas-backend.onrender.com",
+    baseURL: "https://gestordetareasbackend.onrender.com",
 });
 
 //Recibidor de la API para agregar el token automaticamente
